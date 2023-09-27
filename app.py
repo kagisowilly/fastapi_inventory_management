a58b1f52799a76f60d6ab1fd2f2cde8081248328
kagisowilly <mphayiw@gmail.com>
2023-09-27 23:28:52 +0200

    Pydantic Models using Tortoise ORM (Database design)

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI;
+from tortoise.contrib.fastapi import register_tortoise
 
 app = FastAPI()
 
-
 @app.get('/')
 def index():
-    return {"mgs": "Hello world"}
+    return {"Go to /docs for documentation"}
+
+# @app.post("/supplier")
+# def index
+
+register_tortoise(
+    app,
+    db_url="sqlite://database.sqlite3",
+    modules={"models": ["models"]},
+    generate_schemas= True,
+    add_exception_handlers=True
+)
--- models.py
+++ models.py
@@ -7,9 +7,9 @@ class Product(Model):
     name = fields.CharField(max_length=30, nullable = False)
     quantity_in_stock = fields.IntField(default=0)
     quantity_sold = fields.IntField(default=0)
-    unit_price = fields.DecimalField(max_length=8, decimal_places=2, default=0.00 )
-    revenue = fields.DecimalField(max_length=20, decimal_places=2, default=0.00)
-    supplied_by = fields.ForeignKeyField('models.supplier', related_name="goods_supplied")
+    unit_price = fields.DecimalField(max_digits=8, decimal_places=2, default=0.00 )
+    revenue = fields.DecimalField(max_digits=20, decimal_places=2, default=0.00)
+    supplied_by = fields.ForeignKeyField('models.Supplier', related_name="goods_supplied")
 
 class Supplier(Model):
     id= fields.IntField(pk= True) 
@@ -21,5 +21,9 @@ class Supplier(Model):
 # create pydantic model
 # In on pydanticIn is for Input
 product_pydantic = pydantic_model_creator(Product, name="Products")
-product_pydanticIn = pydantic_model_creator(Product, name="Product")
+product_pydanticIn = pydantic_model_creator(Product, name="ProductIn", exclude_readonly=True)
+
+Supplier_pydantic = pydantic_model_creator(Supplier, name="Supplier")
+Supplier_pydanticIn = pydantic_model_creator(Supplier, name="SupplierIn", exclude_readonly=True)
+
     
